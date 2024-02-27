c3d46c6a5f3a001bd7662a7aa6415f9fbc1bd339
ecrujcov <maximusxii_a@hotmail.com>
2024-02-27 14:33:35 -0500

    Revert "Changed slf4j to 2.0.12 as that supports modules, updates modules-info with exports and requires"
    This reverts commit 8a0524f0678f03a58551a0697401fbc371b31f30.

--- easy-rules-core/src/main/java/module-info.java
+++ easy-rules-core/src/main/java/module-info.java
@@ -1,35 +1,3 @@
-/**
- * The MIT License
- *
- *  Copyright (c) 2019, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
 module org.jeasy.rules {
-    opens org.jeasy.rules.api;
-    opens org.jeasy.rules.core;
-    opens org.jeasy.rules.annotation;
-
-    exports org.jeasy.rules.api;
-    exports org.jeasy.rules.core;
-    exports org.jeasy.rules.annotation;
-
-    requires org.slf4j;
-
+    opens org.jeasy.rules;
 }
